a6d5a24e7f61629f9f4ffda98fa109e28435e5ec
jonathan <celluloid-demon@users.noreply.github.com>
2024-10-29 02:12:39 -0700

    tweak

--- env/lenovo-legion.env.bat
+++ env/lenovo-legion.env.bat
@@ -2,7 +2,7 @@
 
 :: Defaults
 
-set DATASET_ROOT=D:\ds.lenovo-legion
+set DATASET_ROOT=C:\mnt\disk-461B34E8-DBBF-46C5-A923-B0D9ADC77551
 set USER=Jonathan
 
 :: Windows paths
@@ -11,5 +11,5 @@ set MY_GAMES=C:\Users\%USER%\Documents\My Games
 
 :: Morrowind
 
-set MORROWIND_VOLUME_OPENMW_CONFIG=%DATASET_ROOT%\morrowind.nt\openmw
+set MORROWIND_VOLUME_OPENMW_CONFIG=%DATASET_ROOT%\morrowind.win\openmw
 set MORROWIND_VOLUME_OPENMW_PATH=%MY_GAMES%\OpenMW
